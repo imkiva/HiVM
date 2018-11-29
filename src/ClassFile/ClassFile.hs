23d00e6f28a3314e2fc463a9aff50808f27ba40d
imkiva <libkernelpanic@gmail.com>
2018-11-30 00:06:54 +0800

    classfile: method and field info

--- src/ClassFile/ClassFile.hs
+++ src/ClassFile/ClassFile.hs
@@ -9,11 +9,19 @@ data ClassConstantPoolEntry = ClassConstantPoolEntry
   }
 
 data ClassFileFieldInfo = ClassFileFieldInfo
-  {
+  { fieldInfoAccessFlag      :: Word16
+  , fieldInfoNameIndex       :: Word16
+  , fieldInfoDescriptorIndex :: Word16
+  , fieldInfoAttributeCount  :: Word16
+  , fieldInfoAttributes      :: [ClassFileAttributeInfo]
   }
 
 data ClassFileMethodInfo = ClassFileMethodInfo
-  {
+  { methodInfoAccessFlag      :: Word16
+  , methodInfoNameIndex       :: Word16
+  , methodInfoDescriptorIndex :: Word16
+  , methodInfoAttributeCount  :: Word16
+  , methodInfoAttributes      :: [ClassFileAttributeInfo]
   }
 
 data ClassFileAttributeInfo = ClassFileAttributeInfo
