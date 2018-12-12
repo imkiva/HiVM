5bc063f9b6ca89f35362893112e074cb6c4844fb
imkiva <libkernelpanic@gmail.com>
2018-12-13 00:02:52 +0800

    state: create java vm

--- src/State/JavaEntrance.hs
+++ src/State/JavaEntrance.hs
@@ -1,9 +1,21 @@
-module State.JavaEntrance where
+module State.JavaEntrance
+  ( runJava
+  ) where
 
+import           ClassPath.ClassLoader
 import           Control.Monad.Except
 import           Control.Monad.State
 import           State.JavaVM
+import qualified Utils.UniqueId        as UID
 
-runJava :: JavaInitArgs -> IO Int
+type ExitCode = Int
+
+runJava :: JavaInitArgs -> IO ExitCode
 runJava initArgs = do
   return 0
+
+createJavaVM :: JavaContext JavaVM
+createJavaVM = return $ JavaVM makeBootstrapClassLoader makeSystemClassLoader makeAppClassLoader []
+
+createThread :: JavaContext JavaVM -> JavaContext JavaThread
+createThread jvm = return $ JavaThread UID.makeUniqueId [] jvm
